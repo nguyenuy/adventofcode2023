c8ff5cfeb94f07dc6bb3a6829bfd54c4e314edd1
Uy Nguyen <nguyenuy@gmail.com>
2023-12-03 23:11:02 -0500

    Finish day03

--- day03.py
+++ day03.py
@@ -27,6 +27,18 @@ def get_adjacent_coordinates(coordinate: Tuple[int, int, int]) -> List[Tuple[int
     
     return coordinate_list
 
+def get_gear_adjacent_coordinates(coordinate: Tuple[int, int]) -> List[Tuple[int, int]]:
+    row = coordinate[0]
+    col = coordinate[1]
+
+    coordinate_list = [(row, col - 1), (row, col + 1)]
+    for column in range(col -1, col + 1 + 1):
+        coordinate_list.append((row - 1, column))
+        coordinate_list.append((row + 1, column))
+
+    
+    return coordinate_list
+
 
 def run_problem_one(prob_input: List[str]) -> int:
     prob_input = pad_input(prob_input)
@@ -47,10 +59,44 @@ def run_problem_one(prob_input: List[str]) -> int:
 
     return total
 
+def build_full_number_coordinate(row: int, value: int, match: Match) -> Dict[Tuple[int, int], int]:
+    x = {}
+    for i in range(match.span()[0], match.span()[1], 1):
+        x[(row, i)] = value
+
+    return x
+
+def run_problem_two(prob_input: List[str]) -> int:
+    total = 0
+    possible_gear_coordinates = set([(row, char_col) for row, string in enumerate(prob_input) for char_col, char in enumerate(string) if char == '*'])
+    full_numeric_data = {}
+
+    # Construct full numeric data map
+    for row, string in enumerate(prob_input):
+        numeric_data = [build_full_number_coordinate(row, int(match.group()), match) for match in re.finditer(r'\b\d+\b', string)]
+        for data in numeric_data:
+            full_numeric_data.update(data)
+    
+    # Check if it's a gear 
+    for gear_coordinate in possible_gear_coordinates:
+        gear_coordinates = get_gear_adjacent_coordinates(gear_coordinate)
+        adjacent_parts = set(gear_coordinates).intersection(set(full_numeric_data.keys()))
+
+        adjacent_parts_values = set([full_numeric_data[part] for part in adjacent_parts])        
+        if len(adjacent_parts_values) == 2:
+            adjacent_parts_values_list = list(adjacent_parts_values)
+            total = total + adjacent_parts_values_list[0] * adjacent_parts_values_list[1]
+
+
+    return total
+
 if __name__ == "__main__":
     day = '03'
     prob_input = read_day(f'{day}a')
     ans = run_problem_one(prob_input)
     print(ans)
 
+    prob_input = read_day(f'{day}a')
+    ans = run_problem_two(prob_input)
+    print(ans)
 
